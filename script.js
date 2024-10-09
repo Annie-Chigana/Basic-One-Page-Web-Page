4deb72b709ee730fc12e1e44f9200e44ebc39ba6
Annie-Chigana <chiganagana.a@gmail.com>
2024-10-09 14:35:46 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+    alert("Thank you for your message, we will get back to you soon!");
+});
